fed810fc135e63afd915fb75472b9ad419e761fe
Ben Stone <benjamin.h.stone@gmail.com>
2020-03-06 11:25:46 -0500

    create storyboard

---
+++ ControlsPractice/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ControlsPractice
+//
+//  Created by Benjamin Stone on 3/6/20.
+//  Copyright © 2020 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func switchValueChanged(_ sender: UISwitch) {
+        
+    }
+    
+}
+
